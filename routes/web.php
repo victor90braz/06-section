eb91d26763ab2676e99c84b99ff549bf9f298541
victor braz <victor.90braz@gmail.com>
2023-10-26 13:57:17 +0200

    //dd(request('search'));

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,10 @@ Route::get('/', function () {
 
     $posts = Post::latest();
 
+    //dd(request('search'));
+
     if (request('search')) {
+
         $posts
             ->where('title', 'like' , '%'. request('search') . '%')
             ->orWhere('body', 'like' , '%'. request('search') . '%');
